f8cb85d4b735f8bc1a96e6cca131494d7dd3b6b3
Soumyadip Sarkar <84954473+soumyadipx11@users.noreply.github.com>
2025-09-28 14:26:41 +0000

    Solved till 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let i;
+    let sum = 0;
+    if(a>0&&b>0&&Number.isInteger(a)&&Number.isInteger(b))
+    {
+        if(b>a)
+        {
+            for(i=a; i<=b; i++)
+                    {
+                        sum=sum+i;
+                    }
+                    return sum;
+        }
+        else
+        {
+            for(i=b; i<=a; i++)
+                    {
+                        sum=sum+i;
+                    }
+                    return sum;
+        }
+        
+    }
+    else
+    {
+        return 'ERROR';
+    }
+
+    
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+  let c=5*(f-32)/9;
+  c=Math.round(c*10)/10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  let f =((9/5)*c)+32;
+  f=Math.round(f*10)/10;
+  return f;
 };
 
 // Do not edit below this line
